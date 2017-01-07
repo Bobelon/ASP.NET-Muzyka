447eed6fecb6129d73c6ca0c9711144d7d9c299e
Filip Wspaniały <Filip Wspaniay>
2017-01-07 15:28:52 +0100

    Dodanie modelu Artist

---
+++ Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusic.Models
+{
+    public class Artist
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string RealName { get; set; }
+        public DateTime BornDate { get; set; }
+        public string Image { get; set; }
+    }
+}
