e065ccd7c866072383851a4c2242566eb2e12f5f
Filip Wspaniały <Filip Wspaniay>
2017-01-07 15:27:26 +0100

    Dodanie modelu Band

---
+++ Models/Band.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusic.Models
+{
+    public class Band
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Debiut { get; set; }
+        public int End { get; set; }
+        public List<Artist> ActualArtists { get; set; }
+        public List<Artist> ExArtists { get; set; }
+        public string Image { get; set; }
+        public List<string> Songs { get; set; }
+    }
+}
