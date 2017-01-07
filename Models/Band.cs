d669fed7177a82d7ef94f52dcff39f03d57f637f
Filip Wspaniały <Filip Wspaniay>
2017-01-07 15:53:18 +0100

    Wyświetlanie się zespołów w postaci listy

--- Models/Band.cs
+++ Models/Band.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,7 @@ namespace MvcMusic.Models
         public int End { get; set; }
         public List<Artist> ActualArtists { get; set; }
         public List<Artist> ExArtists { get; set; }
+        [Display (Name = "Obrazek")]
         public string Image { get; set; }
         public List<string> Songs { get; set; }
     }
